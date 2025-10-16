b2b4f185ef902c4445fa165d0fc750cfbe198bcc
Einar <einarsterten@gmail.com>
2025-10-16 13:10:45 +0200

    adding SFXmanager

---
+++ Assets/_Scripts/Einar/SoundEffectManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using UnityEngine;
+
+public class SoundEffectManager : MonoBehaviour
+{
+
+    [SerializeField] private AudioSource soundFXObject;
+    bool mute = true;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSecondsRealtime(1f);
+        mute = false;
+    }
+    public void PlaySoundFXClip(AudioClip audioClip, Transform spawmTransform, float volume, float duration = 0.0f)
+    {
+        if (mute)
+        {
+            return;
+        }
+        //spawn in GameObject
+        AudioSource audioSource = Instantiate(soundFXObject, spawmTransform.position, Quaternion.identity);
+        //assign audioClip
+        audioSource.clip = audioClip;
+        //assign volume
+        //audioSource.volume = SoundManager.instance.SFXVolumeWithMasterVolumeApplied();
+        //play sound
+        audioSource.Play();
+        //get length of clip
+        float clipLength = audioSource.clip.length;
+
+        //destroy the clip after playing
+        if (duration > 0.0f)
+        {
+            Destroy(audioSource.gameObject, duration);
+        }
+        else
+        {
+            Destroy(audioSource.gameObject, clipLength);
+        }
+    }
+}
